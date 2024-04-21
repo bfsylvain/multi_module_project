eaf4cffa1cfa95194f670c444d319d47bffb0dab
bfsylvain <sylvainbonnaure@hotmail.com>
2024-04-21 18:23:05 +0200

    user changes properly on click

--- src/app/modules/account/components/feature/user-account/user-account.component.ts
+++ src/app/modules/account/components/feature/user-account/user-account.component.ts
@@ -11,38 +11,32 @@ import { switchMap } from 'rxjs';
 })
 export class UserAccountComponent {
 
-  userId!: number
   user!: User
 
   constructor(private appService: AppService, private route: ActivatedRoute){}
 
   ngOnInit(): void {
-    //this.getUserById()
 
-
-    // ECOUTER LE CHANGEMENT DE PARAM DE LA ROUTE
-    // this.route.params.subscribe(param => {
-    //   const userID = param['id']
-    // })
-
-
-    // CREER UN OBSERVABLE SUR LE CHANGEMENT DE ROUTE
-    // PUIS RELANCER LE FILTRE POUR RECUPERER L'UTILISATEUR VOULU
-    this.route.params.pipe(
-      switchMap(params => {
-        const userId = parseInt(params['id']);
-        return this.appService.getUserById(userId);
+    this.route.params.subscribe((params) => {
+      const userId = parseInt(params['id'])
+      this.appService.getUserById(userId)
+      .subscribe(user => {
+        this.user = user
       })
-    ).subscribe(user => {
-      this.user = user;
-      console.log(this.user);
     })
+
+    // METHODE SWITCHMAP ?????
+    // CREER UN OBSERVABLE SUR LE CHANGEMENT DE ROUTE
+    // PUIS LANCER LE FILTRE POUR RECUPERER L'UTILISATEUR VOULU
+    // this.route.params.pipe(
+    //   switchMap(params => {
+    //     const userId = parseInt(params['id']);
+    //     return this.appService.getUserById(userId);
+    //   })
+    // ).subscribe(user => {
+    //   this.user = user;
+    //   console.log(this.user);
+    // })
   }
-  // METHODE POUR FILTRER PAR ID MAIS PBLM PAS DE MISE A JOUR AU CLIC
-  // getUserById(): void {
-  //   this.appService.getAllUsers().subscribe((data)=> {
-  //     this.user = data.filter((el) => (el.id == this.userId))[0]
-  //     console.log(this.user)
-  //   })
-  // }
+
 }
--- src/app/modules/admin/components/feature/user-list-container/user-list-container.component.ts
+++ src/app/modules/admin/components/feature/user-list-container/user-list-container.component.ts
@@ -6,17 +6,19 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-user-list-container',
   templateUrl: './user-list-container.component.html',
-  styleUrl: './user-list-container.component.scss'
+  styleUrl: './user-list-container.component.scss',
 })
 export class UserListContainerComponent {
-userList!: User[];
-  constructor(private appService: AppService){}
+  userList!: User[];
+  constructor(private appService: AppService) {}
 
   ngOnInit(): void {
     this.getUserList();
   }
-  
+
   getUserList() {
-this.appService.getAllUsers().subscribe((data)=> {this.userList = data})
+    this.appService.getAllUsers().subscribe((data) => {
+      this.userList = data;
+    });
   }
 }
