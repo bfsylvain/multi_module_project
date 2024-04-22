cfdf256436380ba078ad9f7051b6d3e0d7aaef0a
bfsylvain <sylvainbonnaure@hotmail.com>
2024-04-22 09:49:39 +0200

    getUserById modified

--- src/app/shared/app.service.ts
+++ src/app/shared/app.service.ts
@@ -3,26 +3,23 @@ import { User } from '../models/classes/user.class';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppService {
+  private _userUrl: string = 'assets/json/users.json';
 
-  private _userUrl: string = "assets/json/users.json"
-  
-  constructor(public httpService: HttpClient) { }
+  constructor(public httpService: HttpClient) {}
 
   getAllUsers(): Observable<User[]> {
-    return this.httpService.get<User[]>(this._userUrl)
+    return this.httpService.get<User[]>(this._userUrl);
   }
 
   // CREER UNE METHODE POUR RECUPERER UN USER PAR SON ID :
   getUserById(userId: number): Observable<User> {
     const foundUser = this.getAllUsers()
-    .pipe(map(users => users.filter(user => user.id === userId)[0]))
-    return foundUser
-    }
-
-
+    .pipe(
+      map((users) => users.find((user) => user.id === userId) as User)
+    );
+    return foundUser;
   }
-
-
+}
